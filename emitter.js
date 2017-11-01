347227268ad2676a5ed58c3ecf3d2cf9c054c71f
tanya <tatiannesterova@gmail.com>
2017-11-02 04:03:34 +0500

    first version

--- emitter.js
+++ emitter.js
@@ -12,6 +12,60 @@ module.exports = getEmitter;
  * @returns {Object}
  */
 function getEmitter() {
+    var definePropertiesForEvent = function (event) {
+        Object.defineProperties(event, {
+            'subscriptions': { value: [], enumerable: false, writable: true },
+            'countEmitted': { value: 0, enumerable: false, writable: true }
+        });
+    };
+
+    var events = {};
+    definePropertiesForEvent(events);
+
+    var offEvent = function (event, context) {
+        event.subscriptions = event.subscriptions.filter(function (subscription) {
+            return subscription.context !== context;
+        });
+        Object.keys(event).forEach(function (subEvent) {
+            offEvent(event[subEvent]);
+        });
+    };
+
+    var backEmit = function (event, subEvents) {
+        if ((subEvents.length !== 0) && event.hasOwnProperty(subEvents[0])) {
+            backEmit(event[subEvents.shift()], subEvents);
+        }
+        event.subscriptions = event.subscriptions.filter(function (subscription) {
+            if (subscription.hasOwnProperty('frequency') &&
+                (event.countEmitted % subscription.frequency) !== 0) {
+                return true;
+            }
+            subscription.handler.call(subscription.context);
+            if (subscription.hasOwnProperty('times')) {
+                if (subscription.times === 1) {
+                    return false;
+                }
+                subscription.times -= 1;
+
+                return true;
+            }
+
+            return true;
+        });
+        event.countEmitted += 1;
+    };
+
+    var getEvent = function (nameEvent) {
+        return nameEvent.split('.').reduce(function (parentEvent, subEvent) {
+            if (!parentEvent.hasOwnProperty(subEvent)) {
+                parentEvent[subEvent] = {};
+                definePropertiesForEvent(parentEvent[subEvent]);
+            }
+
+            return parentEvent[subEvent];
+        }, events);
+    };
+
     return {
 
         /**
@@ -19,26 +73,49 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            this.through(event, context, handler, 1);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            let messageError = 'на event никто не подписан';
+            try {
+                offEvent(event.split('.').reduce(function (parentEvent, subEvent) {
+                    if (!parentEvent.hasOwnProperty(subEvent)) {
+                        throw new TypeError(messageError);
+                    }
+
+                    return parentEvent[subEvent];
+                }, events), context);
+            } catch (error) {
+                if (error.message !== messageError) {
+                    throw error;
+                }
+                console.info(messageError);
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            backEmit(events, event.split('.'));
+
+            return this;
         },
 
         /**
@@ -48,9 +125,15 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            times = times > 0 ? times : 1;
+            getEvent(event).subscriptions.push({ context: context,
+                handler: handler,
+                times: times });
+
+            return this;
         },
 
         /**
@@ -60,9 +143,17 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            if (frequency <= 0) {
+                return this.on(event, context, handler);
+            }
+            getEvent(event).subscriptions.push({ context: context,
+                handler: handler,
+                frequency: frequency });
+
+            return this;
         }
     };
 }
